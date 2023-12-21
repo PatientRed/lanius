8dd0e18dff48e3e8d89895b2ca54b7f55202a5fe
PatientRed <154473346+PatientRed@users.noreply.github.com>
2023-12-21 23:23:11 +0300

    Some examples added

--- lanius/Class1.cs
+++ lanius/Class1.cs
@@ -1,7 +1,37 @@
-﻿namespace lanius
+﻿using System.Diagnostics;
+
+namespace lanius
 {
-    public class Class1
+    public class TelemetryProvider
     {
 
+    }
+
+    internal class Metric
+    {
+        internal int Value { get; private set; }
+        int _first;
+        int _last;
+
+        private int Measure()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            var _process = Process.GetCurrentProcess();
+            var processTime = _process.TotalProcessorTime;
+            var memory = _process.WorkingSet64;
+        }
+
+        public void End()
+        {
+
+        }
+
+        public Metric()
+        {
+        }
     }
 }
