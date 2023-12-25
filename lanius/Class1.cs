1fafedd210917d76df4072ae318e61dfb57491c0
PatientRed <154473346+PatientRed@users.noreply.github.com>
2023-12-25 23:20:29 +0300

    TotalCPUTime alternative comment moved

--- lanius/Class1.cs
+++ lanius/Class1.cs
@@ -49,17 +49,17 @@ namespace lanius
         public override long Value => (long)(_last.TotalMilliseconds - _previous.TotalMilliseconds);
         public override long TotalValue => (long)(_last.TotalMilliseconds - First.TotalMilliseconds);
 
+        //alternative?:
+        //var threads = CurrentProcess.Threads;
+        //TimeSpan threadsTotalTime = new(0);
+        //foreach (ProcessThread thread in threads)
+        //{
+        //    threadsTotalTime += thread.TotalProcessorTime;
+        //}
         protected override Func<TimeSpan> MeasurementMethod() => () => CurrentProcess.TotalProcessorTime;
 
         public override void Measure()
         {
-            //alternative?:
-            //var threads = CurrentProcess.Threads;
-            //TimeSpan threadsTotalTime = new(0);
-            //foreach (ProcessThread thread in threads)
-            //{
-            //    threadsTotalTime += thread.TotalProcessorTime;
-            //}
             throw new NotImplementedException();
         }
 
