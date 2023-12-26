5bdca4d9243c8c5350e1443642e127d04b0a836d
PatientRed <154473346+PatientRed@users.noreply.github.com>
2023-12-26 06:23:13 +0300

    MemoryDelta moved to new file

---
+++ lanius/MemoryDelta.cs
@@ -0,0 +1,13 @@
+﻿namespace lanius
+{
+    internal abstract class MemoryDelta : Metric<long>
+    {
+        public override long Value => _last - _previous;
+        public override long TotalValue => _last - First;
+    }
+
+    internal class WorkingSetDelta : MemoryDelta
+    {
+        protected override Func<long> MeasurementMethod() => () => CurrentProcess.WorkingSet64;
+    }
+}
--- lanius/Metric.cs
+++ lanius/Metric.cs
@@ -47,15 +47,4 @@ namespace lanius
         //}
         protected override Func<TimeSpan> MeasurementMethod() => () => CurrentProcess.TotalProcessorTime;
     }
-
-    internal abstract class MemoryDelta : Metric<long>
-    {
-        public override long Value => _last - _previous;
-        public override long TotalValue => _last - First;
-    }
-
-    internal class WorkingSetDelta : MemoryDelta
-    {
-        protected override Func<long> MeasurementMethod() => () => CurrentProcess.WorkingSet64;
-    }
 }
