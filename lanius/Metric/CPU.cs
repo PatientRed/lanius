4883ee263abd1c9e7c96a208b0c208edf4faa657
PatientRed <154473346+PatientRed@users.noreply.github.com>
2023-12-26 07:09:18 +0300

    TotalCPUTime moved to separate file

---
+++ lanius/Metric/CPU.cs
@@ -0,0 +1,19 @@
+﻿namespace lanius
+{
+    internal class TotalCPUTime : Metric<TimeSpan>
+    {
+        public TimeSpan RawValue => _last - _previous;
+        public TimeSpan RawTotalValue => _last - First;
+        public override long Value => (long)(_last.TotalMilliseconds - _previous.TotalMilliseconds);
+        public override long TotalValue => (long)(_last.TotalMilliseconds - First.TotalMilliseconds);
+
+        //alternative?:
+        //var threads = CurrentProcess.Threads;
+        //TimeSpan threadsTotalTime = new(0);
+        //foreach (ProcessThread thread in threads)
+        //{
+        //    threadsTotalTime += thread.TotalProcessorTime;
+        //}
+        protected override Func<TimeSpan> MeasurementMethod() => () => CurrentProcess.TotalProcessorTime;
+    }
+}
--- lanius/Metric/Metric.cs
+++ lanius/Metric/Metric.cs
@@ -30,21 +30,4 @@ namespace lanius
             First = _previous = _last = MeasurementMethod().Invoke();
         }
     }
-
-    internal class TotalCPUTime : Metric<TimeSpan>
-    {
-        public TimeSpan RawValue => _last - _previous;
-        public TimeSpan RawTotalValue => _last - First;
-        public override long Value => (long)(_last.TotalMilliseconds - _previous.TotalMilliseconds);
-        public override long TotalValue => (long)(_last.TotalMilliseconds - First.TotalMilliseconds);
-
-        //alternative?:
-        //var threads = CurrentProcess.Threads;
-        //TimeSpan threadsTotalTime = new(0);
-        //foreach (ProcessThread thread in threads)
-        //{
-        //    threadsTotalTime += thread.TotalProcessorTime;
-        //}
-        protected override Func<TimeSpan> MeasurementMethod() => () => CurrentProcess.TotalProcessorTime;
-    }
 }
