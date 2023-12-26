b371e22165089fd1b2e746cc9fa346f25c84d752
PatientRed <154473346+PatientRed@users.noreply.github.com>
2023-12-26 04:18:51 +0300

    ITelemetryProvider implemented

---
+++ lanius/ITelemetryProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lanius
+{
+    internal interface ITelemetryProvider
+    {
+        public Dictionary<string, long> Measurements { get; }
+        public void Measure();
+        public void ContinuousMeasure();
+    }
+}
