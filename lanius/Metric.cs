4ab2cfecbb2bb369866e80532e4810f65b4c5981
PatientRed <154473346+PatientRed@users.noreply.github.com>
2023-12-26 06:19:16 +0300

    MemoryDelta abstract metric added

--- lanius/Metric.cs
+++ lanius/Metric.cs
@@ -48,11 +48,14 @@ namespace lanius
         protected override Func<TimeSpan> MeasurementMethod() => () => CurrentProcess.TotalProcessorTime;
     }
 
-    internal class WorkingSetDelta : Metric<long>
+    internal abstract class MemoryDelta : Metric<long>
     {
         public override long Value => _last - _previous;
         public override long TotalValue => _last - First;
+    }
 
+    internal class WorkingSetDelta : MemoryDelta
+    {
         protected override Func<long> MeasurementMethod() => () => CurrentProcess.WorkingSet64;
     }
 }
