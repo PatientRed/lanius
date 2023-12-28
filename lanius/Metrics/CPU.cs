5c77abd180dd4405b80c8a12f61eef5fbcd59cf6
PatientRed <154473346+PatientRed@users.noreply.github.com>
2023-12-28 19:06:53 +0300

    CPUTime.Value calculation fixed

--- lanius/Metrics/CPU.cs
+++ lanius/Metrics/CPU.cs
@@ -5,7 +5,7 @@
         protected override bool RefreshRequired => false;
         public TimeSpan RawValue => _last - _previous;
         public TimeSpan RawTotalValue => _last - First;
-        public override long Value => (long)(_last - _previous).TotalMicroseconds; //Raw.total to long?
+        public override long Value => (long)(_last - _previous).TotalMilliseconds; //Raw.total to long?
         public override long TotalValue => (long)(_last - First).TotalMilliseconds; //Raw.total to long?
     }
 
