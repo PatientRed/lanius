b38d1def858880f6a6ba53b4dcf30eda4e71eda8
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-01-06 18:07:47 +0300

    Added .GetData() extension for IMetric

--- lanius/Measurement.cs
+++ lanius/Measurement.cs
@@ -1,4 +1,10 @@
 ﻿namespace lanius.Measurements
 {
     public readonly record struct Measurement(string Name, long Value, DateTime StartTime, DateTime EndTime);
+
+    public static class Conversions
+    {
+        internal static Measurement GetData(this IMetric metric) =>
+            new() { Name = metric.Name, Value = metric.Value, StartTime = metric.StartTime, EndTime = metric.EndTime };
+    }
 }
