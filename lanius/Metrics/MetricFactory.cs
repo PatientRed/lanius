ce04510b18fa609a3de86f21699f9bf1e4b767b7
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-02-04 12:39:31 +0300

    MetricFactory internally implemented

---
+++ lanius/Metrics/MetricFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace lanius
+{
+    internal static class MetricFactory
+    {
+        public static Dictionary<Type, Func<IMetric>> _constructors =
+            AppDomain.CurrentDomain.GetAssemblies().SelectMany(assembly => assembly.GetTypes())
+                                                   .Where(type => typeof(IMetric).IsAssignableFrom(type) && type.GetConstructors().Where(constructor => constructor.GetParameters().Length == 0).Any())
+                                                   .Select(type => new KeyValuePair<Type, Func<IMetric>>(type, () => (IMetric)type.GetConstructors().First(constructor => constructor.GetParameters().Length == 0).Invoke(null)))
+                                                   .ToDictionary();
+
+        public static IMetric Create(Type type) => _constructors[type].Invoke();
+    }
+}
