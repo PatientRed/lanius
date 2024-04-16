023e2cbba0d3c41287dedbdda8e920f7f457b3c3
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-04-16 19:24:38 +0300

    MetricFactory constructors collection hid

--- lanius/Metrics/MetricFactory.cs
+++ lanius/Metrics/MetricFactory.cs
@@ -2,7 +2,7 @@
 {
     internal static class MetricFactory
     {
-        public static Dictionary<Type, Func<IMetric>> _constructors =
+        private static Dictionary<Type, Func<IMetric>> _constructors =
             AppDomain.CurrentDomain.GetAssemblies().SelectMany(assembly => assembly.GetTypes())
                                                    .Where(type => typeof(IMetric).IsAssignableFrom(type) && type.GetConstructors().Where(constructor => constructor.GetParameters().Length == 0).Any())
                                                    .Select(type => new KeyValuePair<Type, Func<IMetric>>(type, () => (IMetric)type.GetConstructors().First(constructor => constructor.GetParameters().Length == 0).Invoke(null)))
