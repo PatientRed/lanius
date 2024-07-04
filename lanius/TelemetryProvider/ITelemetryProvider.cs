4efea69ad04eac25b2b1bc99694d1bd79d571248
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-07-04 16:04:49 +0300

    ITelemetryProvider extended with static factory method

--- lanius/TelemetryProvider/ITelemetryProvider.cs
+++ lanius/TelemetryProvider/ITelemetryProvider.cs
@@ -2,8 +2,11 @@
 
 namespace lanius
 {
-    internal interface ITelemetryProvider
+    public interface ITelemetryProvider<T, U> where T : ITelemetryProvider<T, U>
+                                              where U : IMetric
     {
+        public static abstract T CreateProvider(IEnumerable<Type> metrics, IMetricFactory<U> factory, IDataStorageProvider? storageProvider = null);
+
         public IEnumerable<Measurement> Measurements { get; }
         public void Measure();
         public void ContinuousMeasure();
