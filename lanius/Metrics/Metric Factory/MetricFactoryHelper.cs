0142e93ae3b70b25f039f56c32ad54a4b437c043
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-10-09 19:00:40 +0300

    MetricFactoryHelper implemented to reduce boilerplate reflection

---
+++ lanius/Metrics/Metric Factory/MetricFactoryHelper.cs
@@ -0,0 +1,13 @@
+﻿using lanius.Metrics;
+using System.Reflection;
+
+namespace lanius.MetricFactories
+{
+    public static class MetricFactoryHelper
+    {
+        public static IEnumerable<ConstructorInfo> GetConstructors<T>() where T : IMetric =>
+            AppDomain.CurrentDomain.GetAssemblies().SelectMany(assembly => assembly.GetTypes())
+                                                   .Where(type => typeof(T).IsAssignableFrom(type))
+                                                   .SelectMany(type => type.GetConstructors().Where(ci => ci.DeclaringType is not null && !ci.DeclaringType.IsAbstract && !ci.IsStatic && ci.IsPublic));
+    }
+}
