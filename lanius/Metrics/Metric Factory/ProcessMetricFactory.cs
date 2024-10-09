66c6f24cfbf3e4dbb2984b80db01b802ecd6e9bb
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-10-09 22:24:09 +0300

    Linq indentation inside PMF original changed to fit newer revisions

--- lanius/Metrics/Metric Factory/ProcessMetricFactory.cs
+++ lanius/Metrics/Metric Factory/ProcessMetricFactory.cs
@@ -15,11 +15,12 @@ namespace lanius.MetricFactories
 
     public class ProcessMetricFactoryOriginal : IMetricFactory<IProcessMetric>
     {
-        private static readonly Dictionary<Type, Func<int, IProcessMetric>> _constructors = MetricFactoryHelper.GetConstructors<IProcessMetric>()
-                                                                                .Where(ci => ci.GetParameters().Length == 1 && ci.GetParameters()[0].ParameterType == typeof(int))
-                                                                                //.DeclaringType is nullchecked inside MetricFactoryHelper.GetConstructors
-                                                                                .Select(ci => new KeyValuePair<Type, Func<int, IProcessMetric>>(ci.DeclaringType!, (input) => (IProcessMetric)ci.Invoke([input])))
-                                                                                .ToDictionary();
+        private static readonly Dictionary<Type, Func<int, IProcessMetric>> _constructors =
+            MetricFactoryHelper.GetConstructors<IProcessMetric>()
+            .Where(ci => ci.GetParameters().Length == 1 && ci.GetParameters()[0].ParameterType == typeof(int))
+            //.DeclaringType is nullchecked inside MetricFactoryHelper.GetConstructors
+            .Select(ci => new KeyValuePair<Type, Func<int, IProcessMetric>>(ci.DeclaringType!, (input) => (IProcessMetric)ci.Invoke([input])))
+            .ToDictionary();
 
         public static ProcessMetricFactoryOriginal GetFactory(int processID) => new ProcessMetricFactoryOriginal(processID);
 
