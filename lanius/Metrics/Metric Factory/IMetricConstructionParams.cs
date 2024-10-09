a5920fce08bf9068405a8cbe19dcb09ec43c50c1
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-10-09 21:15:12 +0300

    IMetricConstructionParams implemented to simplify constructors selector for *MFs

---
+++ lanius/Metrics/Metric Factory/IMetricConstructionParams.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+namespace lanius.MetricFactories.ConstructionParameters
+{
+    public interface IMetricConstructionParams
+    {
+        static abstract Type[] Params { get; }
+    }
+
+    public static class ConstructionParamsExtensions
+    {
+        public static bool IsCompatible<T>(this ParameterInfo[] actual) where T : IMetricConstructionParams
+        {
+            if (T.Params.Length != actual.Length)
+                return false;
+
+            for (int i = 0; i < T.Params.Length; i++)
+                //TODO: equality comparison instead?
+                if (actual[i].ParameterType.IsAssignableFrom(T.Params[i]))
+                    return false;
+
+            return true;
+        }
+    }
+}
