7636ac1ba06a12a1662bbd09e4125a730aa5c8f9
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-10-09 22:12:33 +0300

    BaseMetricFactoryParametrized implemented to help successors get rid of helper class calls

---
+++ lanius/Metrics/Metric Factory/Parametrized/BaseMetricFactoryParametrized.cs
@@ -0,0 +1,11 @@
+﻿using lanius.Metrics;
+using System.Reflection;
+
+namespace lanius.MetricFactories.Parametrized
+{
+    public abstract class BaseMetricFactoryParametrized<T, U> where T : IMetric
+                                                              where U : IMetricConstructionParams
+    {
+        protected static IEnumerable<ConstructorInfo> GetConstructors() => MetricFactoryHelper.GetConstructors<T, U>();
+    }
+}
