65d0d92583c8259e2aa0f07431f5eb849723afb8
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-10-12 14:00:20 +0300

    CPMF new implementation flow

--- lanius/Metrics/Metric Factory/CurrentProcessMetricFactory.cs
+++ lanius/Metrics/Metric Factory/CurrentProcessMetricFactory.cs
@@ -1,21 +1,21 @@
-﻿using lanius.Metrics;
+﻿using lanius.MetricFactories.Parametrized;
+using lanius.Metrics;
 
 namespace lanius.MetricFactories
 {
-    public class CurrentProcessMetricFactory : IMetricFactory<ICurrentProcessMetric>
+    public class CurrentProcessMetricFactory : BaseMetricFactoryParametrized<ICurrentProcessMetric, EmptyMetricConstructionParams>
     {
         private static readonly Dictionary<Type, Func<ICurrentProcessMetric>> _constructors =
-            MetricFactoryHelper.GetConstructors<ICurrentProcessMetric>()
-                               .Where(ci => ci.GetParameters().Length == 0)
-                               //.DeclaringType is nullchecked inside MetricFactoryHelper.GetConstructors
-                               .Select(ci => new KeyValuePair<Type, Func<ICurrentProcessMetric>>(ci.DeclaringType!, () => (ICurrentProcessMetric)ci.Invoke(null)))
-                               .ToDictionary();
+            GetConstructors()
+            //.DeclaringType is nullchecked inside MetricFactoryHelper.GetConstructors
+            .Select(ci => new KeyValuePair<Type, Func<ICurrentProcessMetric>>(ci.DeclaringType!, () => (ICurrentProcessMetric)ci.Invoke(null)))
+            .ToDictionary();
 
         private static CurrentProcessMetricFactory? _this = null;
 
         public static CurrentProcessMetricFactory GetFactory() => _this ??= new CurrentProcessMetricFactory();
 
-        public ICurrentProcessMetric Create(Type type) => _constructors[type].Invoke();
+        public override ICurrentProcessMetric Create(Type type) => _constructors[type].Invoke();
 
         private CurrentProcessMetricFactory() { }
     }
