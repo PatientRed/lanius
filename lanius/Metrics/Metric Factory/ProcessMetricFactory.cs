0732cccb9302bcacfebcaca553f5135634751e15
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-10-12 12:15:01 +0300

    The only PMF version remaining, no helper calls, simplified

--- lanius/Metrics/Metric Factory/ProcessMetricFactory.cs
+++ lanius/Metrics/Metric Factory/ProcessMetricFactory.cs
@@ -13,24 +13,6 @@ namespace lanius.MetricFactories
         private ProcessMetricParams() { }
     }
 
-    public class ProcessMetricFactoryOriginal : IMetricFactory<IProcessMetric>
-    {
-        private static readonly Dictionary<Type, Func<int, IProcessMetric>> _constructors =
-            MetricFactoryHelper.GetConstructors<IProcessMetric>()
-            .Where(ci => ci.GetParameters().Length == 1 && ci.GetParameters()[0].ParameterType == typeof(int))
-            //.DeclaringType is nullchecked inside MetricFactoryHelper.GetConstructors
-            .Select(ci => new KeyValuePair<Type, Func<int, IProcessMetric>>(ci.DeclaringType!, (input) => (IProcessMetric)ci.Invoke([input])))
-            .ToDictionary();
-
-        public static ProcessMetricFactoryOriginal GetFactory(int processID) => new ProcessMetricFactoryOriginal(processID);
-
-        public IProcessMetric Create(Type type) => _constructors[type].Invoke(_processId);
-
-        private readonly int _processId;
-
-        internal ProcessMetricFactoryOriginal(int id) => _processId = id;
-    }
-
     public class ProcessMetricFactoryAlt2 : BaseMetricFactoryParametrized<IProcessMetric, ProcessMetricParams>,
                                             IMetricFactory<IProcessMetric>
     {
@@ -47,22 +29,5 @@ namespace lanius.MetricFactories
         private readonly int _processId;
 
         internal ProcessMetricFactoryAlt2(int id) => _processId = id;
-    }
-
-    public class ProcessMetricFactoryAlt : IMetricFactory<IProcessMetric>
-    {
-        private static readonly Dictionary<Type, Func<int, IProcessMetric>> _constructors =
-            MetricFactoryHelper.GetConstructors<IProcessMetric, ProcessMetricParams>()
-            //.DeclaringType is nullchecked inside MetricFactoryHelper.GetConstructors
-            .Select(ci => new KeyValuePair<Type, Func<int, IProcessMetric>>(ci.DeclaringType!, (input) => (IProcessMetric)ci.Invoke([input])))
-            .ToDictionary();
-
-        public static ProcessMetricFactoryAlt GetFactory(int processID) => new ProcessMetricFactoryAlt(processID);
-
-        public IProcessMetric Create(Type type) => _constructors[type].Invoke(_processId);
-
-        private readonly int _processId;
-
-        internal ProcessMetricFactoryAlt(int id) => _processId = id;
     }
 }
