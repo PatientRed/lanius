e1bc873055b6ddc6407566c534113c14c3a36ad6
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-12-15 04:50:23 +0300

    EmptyMetricConstructionParams extracted into separate file

--- lanius/Metrics/Metric Factory/Parametrized/BaseMetricFactoryParametrized.cs
+++ lanius/Metrics/Metric Factory/Parametrized/BaseMetricFactoryParametrized.cs
@@ -3,13 +3,6 @@ using System.Reflection;
 
 namespace lanius.MetricFactories.Parametrized
 {
-    public sealed class EmptyMetricConstructionParams : IMetricConstructionParams
-    {
-        public static Type[] Params => [];
-
-        private EmptyMetricConstructionParams() { }
-    }
-
     public abstract class BaseMetricFactoryParametrized<T, U> : IMetricFactory<T> where T : IMetric
                                                                                   where U : IMetricConstructionParams
     {
---
+++ lanius/Metrics/Metric Factory/Parametrized/EmptyMetricConstructionParams.cs
@@ -0,0 +1,9 @@
+﻿namespace lanius.MetricFactories.Parametrized
+{
+    public sealed class EmptyMetricConstructionParams : IMetricConstructionParams
+    {
+        public static Type[] Params => [];
+
+        private EmptyMetricConstructionParams() { }
+    }
+}
